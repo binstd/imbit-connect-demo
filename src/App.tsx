65e3cb93b043a98af65bf7b7aeac6ff9424d02e9
ethluz <lvxing67@126.com>
2019-03-21 11:33:39 +0800

    connectdemo

---
+++ src/App.tsx
@@ -0,0 +1,520 @@
+import * as React from "react";
+import styled from "styled-components";
+import WalletConnect from "@walletconnect/browser";
+import WalletConnectQRCodeModal from "@walletconnect/qrcode-modal";
+import { IInternalEvent } from "@walletconnect/types";
+import Button from "./components/Button";
+import Column from "./components/Column";
+import Wrapper from "./components/Wrapper";
+import Modal from "./components/Modal";
+import Header from "./components/Header";
+import Loader from "./components/Loader";
+import { fonts } from "./styles";
+import {
+  apiGetAccountAssets,
+  apiGetGasPrices,
+  apiGetAccountNonce
+} from "./helpers/api";
+// import {
+//   recoverTypedSignature
+// } from "./helpers/ethSigUtil";
+import { sanitizeHex, ecrecover } from "./helpers/utilities";
+import {
+  convertAmountToRawNumber,
+  convertStringToHex
+} from "./helpers/bignumber";
+import { IAssetData } from "./helpers/types";
+import Banner from "./components/Banner";
+import AccountAssets from "./components/AccountAssets";
+
+const SLayout = styled.div`
+  position: relative;
+  width: 100%;
+  /* height: 100%; */
+  min-height: 100vh;
+  text-align: center;
+`;
+
+const SContent = styled(Wrapper)`
+  width: 100%;
+  height: 100%;
+  padding: 0 16px;
+`;
+
+const SLanding = styled(Column)`
+  height: 600px;
+`;
+
+const SButtonContainer = styled(Column)`
+  width: 250px;
+  margin: 50px 0;
+`;
+
+const SConnectButton = styled(Button)`
+  border-radius: 8px;
+  font-size: ${fonts.size.medium};
+  height: 44px;
+  width: 100%;
+  margin: 12px 0;
+`;
+
+const SContainer = styled.div`
+  height: 100%;
+  min-height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  word-break: break-word;
+`;
+
+const SModalTitle = styled.div`
+  margin: 1em 0;
+  font-size: 20px;
+  font-weight: 700;
+`;
+
+const SModalParagraph = styled.p`
+  margin-top: 30px;
+`;
+
+const SBalances = styled(SLanding)`
+  height: 100%;
+  & h3 {
+    padding-top: 30px;
+  }
+`;
+
+const STable = styled(SContainer)`
+  flex-direction: column;
+  text-align: left;
+`;
+
+const SRow = styled.div`
+  width: 100%;
+  display: flex;
+  margin: 6px 0;
+`;
+
+const SKey = styled.div`
+  width: 30%;
+  font-weight: 700;
+`;
+
+const SValue = styled.div`
+  width: 70%;
+  font-family: monospace;
+`;
+
+const STestButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const STestButton = styled(Button)`
+  border-radius: 8px;
+  font-size: ${fonts.size.medium};
+  height: 44px;
+  width: 100%;
+  margin: 12px;
+`;
+
+interface IAppState {
+  walletConnector: WalletConnect | null;
+  fetching: boolean;
+  connected: boolean;
+  chainId: number;
+  showModal: boolean;
+  pendingRequest: boolean;
+  uri: string;
+  accounts: string[];
+  address: string;
+  result: any | null;
+  assets: IAssetData[];
+}
+
+const INITIAL_STATE: IAppState = {
+  walletConnector: null,
+  fetching: false,
+  connected: false,
+  chainId: 1,
+  showModal: false,
+  pendingRequest: false,
+  uri: "",
+  accounts: [],
+  address: "",
+  result: null,
+  assets: []
+};
+
+class App extends React.Component<any, any> {
+  public state: IAppState = {
+    ...INITIAL_STATE
+  };
+
+  public walletConnectInit = async () => {
+    // bridge url
+    const bridge = "https://bridge.walletconnect.org";
+
+    // create new walletConnector
+    const walletConnector = new WalletConnect({ bridge });
+
+    window.walletConnector = walletConnector;
+
+    await this.setState({ walletConnector });
+
+    // check if already connected
+    if (!walletConnector.connected) {
+      // create new session
+      await walletConnector.createSession();
+
+      // get uri for QR Code modal
+      const uri = walletConnector.uri;
+
+      // console log the uri for development
+      console.log(uri); // tslint:disable-line
+
+      // display QR Code modal
+      WalletConnectQRCodeModal.open(uri, () => {
+        console.log("QR Code Modal closed"); // tslint:disable-line
+      });
+    }
+    // subscribe to events
+    await this.subscribeToEvents();
+  };
+
+  public subscribeToEvents = () => {
+    const { walletConnector } = this.state;
+
+    if (!walletConnector) {
+      return;
+    }
+
+    walletConnector.on("session_update", async (error, payload) => {
+      console.log('walletConnector.on("session_update")'); // tslint:disable-line
+
+      if (error) {
+        throw error;
+      }
+
+      const { chainId, accounts } = payload.params[0];
+      this.onSessionUpdate(accounts, chainId);
+    });
+
+    walletConnector.on("connect", (error, payload) => {
+      console.log('walletConnector.on("connect")'); // tslint:disable-line
+
+      if (error) {
+        throw error;
+      }
+
+      this.onConnect(payload);
+    });
+
+    walletConnector.on("disconnect", (error, payload) => {
+      console.log('walletConnector.on("disconnect")'); // tslint:disable-line
+
+      if (error) {
+        throw error;
+      }
+
+      this.resetApp();
+    });
+
+    if (walletConnector.connected) {
+      const { chainId, accounts } = walletConnector;
+      const address = accounts[0];
+      this.setState({
+        connected: true,
+        chainId,
+        accounts,
+        address
+      });
+    }
+
+    this.setState({ walletConnector });
+  };
+
+  public killSession = async () => {
+    const { walletConnector } = this.state;
+    if (walletConnector) {
+      walletConnector.killSession();
+    }
+    this.resetApp();
+  };
+
+  public resetApp = async () => {
+    await this.setState({ ...INITIAL_STATE });
+  };
+
+  public onConnect = async (payload: IInternalEvent) => {
+    const { chainId, accounts } = payload.params[0];
+    const address = accounts[0];
+    await this.setState({
+      connected: true,
+      chainId,
+      accounts,
+      address
+    });
+    WalletConnectQRCodeModal.close();
+    this.getAccountAssets();
+  };
+
+  public onSessionUpdate = async (accounts: string[], chainId: number) => {
+    const address = accounts[0];
+    await this.setState({ chainId, accounts, address });
+    await this.getAccountAssets();
+  };
+
+  public getAccountAssets = async () => {
+    const { address, chainId } = this.state;
+    this.setState({ fetching: true });
+    try {
+      // get account balances
+      const assets = await apiGetAccountAssets(address, chainId);
+
+      await this.setState({ fetching: false, address, assets });
+    } catch (error) {
+      console.error(error); // tslint:disable-line
+      await this.setState({ fetching: false });
+    }
+  };
+
+  public toggleModal = () =>
+    this.setState({ showModal: !this.state.showModal });
+
+  public testSendTransaction = async () => {
+    const { walletConnector, address, chainId } = this.state;
+
+    if (!walletConnector) {
+      return;
+    }
+
+    // from
+    const from = address;
+
+    // to
+    const to = address;
+
+    // nonce
+    const _nonce = await apiGetAccountNonce(address, chainId);
+    const nonce = sanitizeHex(convertStringToHex(_nonce));
+
+    // gasPrice
+    const gasPrices = await apiGetGasPrices();
+    const _gasPrice = gasPrices.slow.price;
+    const gasPrice = sanitizeHex(
+      convertStringToHex(convertAmountToRawNumber(_gasPrice, 9))
+    );
+
+    // gasLimit
+    const _gasLimit = 21000;
+    const gasLimit = sanitizeHex(convertStringToHex(_gasLimit));
+
+    // value
+    const _value = 0;
+    const value = sanitizeHex(convertStringToHex(_value));
+
+    // data
+    const data = "0x";
+
+    // test transaction
+    const tx = {
+      from,
+      to,
+      nonce,
+      gasPrice,
+      gasLimit,
+      value,
+      data
+    };
+
+    try {
+      // open modal
+      this.toggleModal();
+
+      // toggle pending request indicator
+      this.setState({ pendingRequest: true });
+
+      // send transaction
+      const result = await walletConnector.sendTransaction(tx);
+
+      // format displayed result
+      const formattedResult = {
+        method: "eth_sendTransaction",
+        txHash: result,
+        from: address,
+        to: address,
+        value: "0 ETH"
+      };
+
+      // display result
+      this.setState({
+        walletConnector,
+        pendingRequest: false,
+        result: formattedResult || null
+      });
+    } catch (error) {
+      console.error(error); // tslint:disable-line
+      this.setState({ walletConnector, pendingRequest: false, result: null });
+    }
+  };
+
+  public testSignMessage = async () => {
+    const { walletConnector, address } = this.state;
+
+    if (!walletConnector) {
+      return;
+    }
+
+    // test message
+    const msgParams = [address, "My email is john@doe.com - 1537836206101"];
+
+    try {
+      // open modal
+      this.toggleModal();
+
+      // toggle pending request indicator
+      this.setState({ pendingRequest: true });
+
+      // send message
+      const result = await walletConnector.signMessage(msgParams);
+
+      // verify signature
+      const signer = ecrecover(result, msgParams[1]);
+      const verified = signer.toLowerCase() === address.toLowerCase();
+
+      // format displayed result
+      const formattedResult = {
+        method: "eth_sign",
+        address,
+        signer,
+        verified,
+        result
+      };
+
+      // display result
+      this.setState({
+        walletConnector,
+        pendingRequest: false,
+        result: formattedResult || null
+      });
+    } catch (error) {
+      console.error(error); // tslint:disable-line
+      this.setState({ walletConnector, pendingRequest: false, result: null });
+    }
+  };
+
+
+  public render = () => {
+    const {
+      assets,
+      address,
+      connected,
+      chainId,
+      fetching,
+      showModal,
+      pendingRequest,
+      result
+    } = this.state;
+    return (
+      <SLayout>
+        <Column maxWidth={1000} spanHeight>
+          <Header
+            connected={connected}
+            address={address}
+            chainId={chainId}
+            killSession={this.killSession}
+          />
+          <SContent>
+            {!address && !assets.length ? (
+              <SLanding center>
+                <h3>
+                  {`尝试使用WalletConnect`}
+                  <br />
+                  {/* <span>{`v${process.env.REACT_APP_VERSION}`}</span> */}
+                </h3>
+                <SButtonContainer>
+                  <SConnectButton
+                    left
+                    onClick={this.walletConnectInit}
+                    fetching={fetching}
+                  >
+                    {"建立连接"}
+                  </SConnectButton>
+                </SButtonContainer>
+              </SLanding>
+            ) : (
+              <SBalances>
+                <Banner />
+                <h3>操作</h3>
+                <Column center>
+                  <STestButtonContainer>
+                    <STestButton left onClick={this.testSendTransaction}>
+                      {"Send Test Transaction"}
+                    </STestButton>
+
+                    <STestButton left onClick={this.testSignMessage}>
+                      {"Sign Test Message"}
+                    </STestButton>
+
+                    <STestButton
+                      disabled
+                      left
+                      // onClick={this.testSignTypedData}
+                    >
+                      {"Sign Test Typed Data"}
+                    </STestButton>
+                  </STestButtonContainer>
+                </Column>
+                <h3>余额</h3>
+                {!fetching ? (
+                  <AccountAssets chainId={chainId} assets={assets} />
+                ) : (
+                  <Column center>
+                    <SContainer>
+                      <Loader />
+                    </SContainer>
+                  </Column>
+                )}
+              </SBalances>
+            )}
+          </SContent>
+        </Column>
+        <Modal show={showModal} toggleModal={this.toggleModal}>
+          {pendingRequest ? (
+            <div>
+              <SModalTitle>{"呼叫请求"}</SModalTitle>
+              <SContainer>
+                <Loader />
+                <SModalParagraph>
+                  {"请使用imbit钱包确认或拒绝操作"}
+                </SModalParagraph>
+              </SContainer>
+            </div>
+          ) : result ? (
+            <div>
+              <SModalTitle>{"请求被确认"}</SModalTitle>
+              <STable>
+                {Object.keys(result).map(key => (
+                  <SRow key={key}>
+                    <SKey>{key}</SKey>
+                    <SValue>{result[key].toString()}</SValue>
+                  </SRow>
+                ))}
+              </STable>
+            </div>
+          ) : (
+            <div>
+              <SModalTitle>{"请求被拒绝"}</SModalTitle>
+            </div>
+          )}
+        </Modal>
+      </SLayout>
+    );
+  };
+}
+
+export default App;
---
+++ src/components/Banner.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import styled from "styled-components";
+import banner from "../assets/ic_launcher.png";
+
+const SBannerWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+`;
+
+const SBanner = styled.div`
+  width: 50px;
+  height: 50px;
+  background: url(${banner}) no-repeat;
+  background-size: cover;
+  background-position: center;
+`;
+
+const Banner = () => (
+  <SBannerWrapper>
+    <SBanner />
+  </SBannerWrapper>
+);
+
+export default Banner;
