7eb283f52c281fc9c51f5d0ceacade0a16a54234
ethluz <lvxing67@126.com>
2019-03-25 09:49:36 +0800

    luz

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import styled from "styled-components";
 import WalletConnect from "@walletconnect/browser";
-import WalletConnectQRCodeModal from "@walletconnect/qrcode-modal";
+import WalletConnectQRCodeModal from "qrcode-modal";
 import { IInternalEvent } from "@walletconnect/types";
 import Button from "./components/Button";
 import Column from "./components/Column";
